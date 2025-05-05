73ad0c5cdce51fb3c84396c2c6ca85b89a421957
Gustaw <gustaw.andrzejewski@gmail.com>
2025-05-05 22:33:59 +0200

    update training script

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import lightning as L
 import torch
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
@@ -8,31 +10,33 @@ from model import DinoMarsFormer, LitDinoMarsFormer
 
 
 def main(
-    batch_size: int = 64,
+    batch_size: int = 128,
     image_size: int = 224,
     validation_split: float = 0.1,
-    learning_rate: float = 1e-5,
+    learning_rate: float = 1e-4,
     weight_decay: float = 1e-4,
-    num_epochs: int = 200,
-    decoder_embedding_dim: int = 256,
+    num_epochs: int = 50,
+    decoder_type: Literal["convnext", "mlp"] = "convnext",
+    decoder_embedding_dim: int = 56,
+    decoder_activation: Literal["gelu", "relu", "leaky_relu"] = "gelu",
     selected_layers: list = [3, 6, 9, 12],
     backbone_name: str = "dinov2_vits14",
     save_dir: str = "checkpoints/",
-    patience: int = 50,
+    patience: int = 10,
 ):
     print(f"Loading backbone: {backbone_name}...")
     dino_backbone = torch.hub.load("facebookresearch/dinov2", backbone_name)
 
     model = DinoMarsFormer(
-        dino_backbone=dino_backbone,
+        backbone=dino_backbone,
         selected_layers=selected_layers,
         embed_dim=decoder_embedding_dim,
         num_classes=len(CLASS_LABELS),
         image_size=image_size,
+        decoder_type=decoder_type,
+        activation=decoder_activation,
         freeze_backbone=True,
     )
-
-    model.count_parameters()
 
     lit_model = LitDinoMarsFormer(
         model=model,
